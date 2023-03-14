042d39d9108428b879f2fe97840f79df059c48dc
terwer <youweics@163.com>
2023-03-15 01:08:40 +0800

    fix: fix build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,14 +2,12 @@
 
 ## [1.0.1](https://github.com/terwer/zhi-common/compare/v1.0.0...v1.0.1) (2023-03-14)
 
-
 ### Bug Fixes
 
-* fix package name ([8e22486](https://github.com/terwer/zhi-common/commit/8e22486ff088e2ad5ba64362afaa69a9c80aaff8))
+- fix package name ([8e22486](https://github.com/terwer/zhi-common/commit/8e22486ff088e2ad5ba64362afaa69a9c80aaff8))
 
 ## 1.0.0 (2023-03-14)
 
-
 ### Features
 
-* Init zhi-common ([1312c6d](https://github.com/terwer/zhi-common/commit/1312c6d4bf4d78d25aced2baa9f0cbb46076e563))
+- Init zhi-common ([1312c6d](https://github.com/terwer/zhi-common/commit/1312c6d4bf4d78d25aced2baa9f0cbb46076e563))
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
 /**
  * @packageDocumentation
- * Hello World
+ * 公共组件
  */
 
-import ZhiUtil from "~/src/utils/ZhiUtil"
 import ThemeFromEnum from "~/src/enums/themeFromEnum"
+import ZhiSdk from "zhi-sdk"
+import Env from "zhi-env"
+
+/**
+ * 工具类统一入口
+ *
+ * @public
+ * @author terwer
+ * @since 1.0.0
+ */
+class ZhiUtil {
+  private static zhiSdkObj: ZhiSdk
+
+  /**
+   * 获取 zhi-sdk 实例
+   *
+   * @param env - 可选，环境变量对象
+   */
+  public static zhiSdk(env?: Env) {
+    if (!ZhiUtil.zhiSdkObj) {
+      ZhiUtil.zhiSdkObj = new ZhiSdk(env)
+      /**
+       * 日志
+       */
+      const logger = ZhiUtil.zhiSdkObj.getLogger()
+      /**
+       * 公共类库
+       */
+      const common = ZhiUtil.zhiSdkObj.common
+      logger.info(common.strUtil.f("ZhiSdk inited, components are available now,like logger, env and so on."))
+    }
+    return ZhiUtil.zhiSdkObj
+  }
+}
+
 export default ZhiUtil
 export { ThemeFromEnum }
