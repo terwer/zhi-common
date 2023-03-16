2220d6d6b559f4ba10781b80c1170492f27d3a87
terwer <youweics@163.com>
2023-03-16 13:28:27 +0800

    feat: update docs

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,14 @@
 import { describe, it } from "vitest"
+import ZhiUtil from "~/src/index"
+import Env from "zhi-env"
 
-describe("test index", () => {
-  it("test hello", function () {
-    console.log("hello")
+describe("test common", () => {
+  it("test zhiSdk", function () {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const env = new Env(import.meta.env)
+    const zhiUtil = ZhiUtil.zhiSdk(env)
+    const now = zhiUtil.common.dateUtil.nowDateZh()
+    console.log("now=>", now)
   })
 })
